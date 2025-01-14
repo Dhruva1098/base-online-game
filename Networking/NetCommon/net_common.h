3f21b9aa5f0f509f41cda5f4ee28fc96c8da2829
Dhruva Sharma <dhruva1098@gmail.com>
2025-01-14 14:06:16 +0530

    added the message structure

---
+++ Networking/NetCommon/net_common.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <memory>
+#include <mutex>
+#include <deque>
+#include <vector>
+#include <thread>
+#include <algorithm>
+#include <cstdint>
+
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+
---
+++ Networking/NetCommon/net_message.h
@@ -0,0 +1,66 @@
+// Defination of our message object
+#pragma once
+#include "net_common.h"
+
+template <typename T>
+struct message_header {
+	T id{};
+	uint32_t size = 0;
+};
+
+template <typename T>
+struct message {
+	message_header<T> header{};
+	std::vector<uint8_t> body;
+
+	// return size of message packet
+	size_t size() const {
+		return sizeof(message_header<T>) + body.size();
+	}
+
+	// Override for std::cout compatibility
+	friend std::ostream& operator << (std::ostream& os, const message<T>& msg) {
+		os << "ID:" << int(msg.header.id) << " Size:" << msg.header.size;
+		return os;
+	}
+
+	// Push using <<
+	template<typename DataType>  // Because we want to be able to puch any data type
+	friend message<T>& operator << (message<T>& msg, const DataType& data) {
+		// check if type provided is copyable
+		static_assert(std::is_standard_layout<DataType>::value, "Data is too complex for a vector");
+
+		// cache current suze of vector, this is where we insert data
+		size_t i = msg.body.size();
+
+		// Resize vector
+		msg.body.resize(msg.body.size() + sizeof(DataType));
+
+		// copy data into newly allocated space
+		std::memcpy(msg.body.data() + i, &data, sizeof(DataType));
+
+		// update size 
+		msg.header.size = msg.size();
+
+		return msg;
+	}
+
+	// Pop using >> 
+	template<typename DataType>
+	friend message<T>& operator >> (message<T>& msg, DataType& data) {
+		static_assert(std::is_standard_layout<DataType>::value, "data is too complex");
+
+		// cache to where our data starts
+		size_t i = msg.body.size() - sizeof(DataType);
+
+		// copy
+		std::memcpy(&data, msg.body.data() + i, sizeof(DataType));
+
+		// shrink vector
+		msg.body.resize(i);
+
+		return msg;
+
+	}
+};
+
