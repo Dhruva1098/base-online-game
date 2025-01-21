8d873788ad3da006605c22d10416e4b7e1a1689d
Dhruva Sharma <dhruva1098@gmail.com>
2025-01-21 12:47:20 +0530

    minor changes to SimpleClient

--- Networking/NetClient/SimpleClient.cpp
+++ Networking/NetClient/SimpleClient.cpp
@@ -3,106 +3,122 @@
 #include <chrono>
 #include <olc.h>
 
-enum class CustomMsgTypes : uint32_t {
-    ServerAccept,
-    ServerDeny,
-    ServerPing,
-    MessageAll,
-    ServerMessage
+
+enum class CustomMsgTypes : uint32_t
+{
+	ServerAccept,
+	ServerDeny,
+	ServerPing,
+	MessageAll,
+	ServerMessage,
 };
 
-class CustomClient : public client_interface<CustomMsgTypes> {
+
+
+class CustomClient : public client_interface<CustomMsgTypes>
+{
 public:
-    void PingServer() {
-        std::cout << "PingServer called\n"; // Debug message
-        message<CustomMsgTypes> msg;
-        msg.header.id = CustomMsgTypes::ServerPing;
-        std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
-        msg << timeNow;
-        Send(msg);
-        std::cout << "Ping message sent to server.\n"; // Debug message
-    }
-    void MessageAll() {
-        std::cout << "MEssage All Called\n";
-        message<CustomMsgTypes> msg;
-        msg.header.id = CustomMsgTypes::MessageAll;
-        Send(msg);
-        std::cout << "Message sent to server\n";
-    }
+	void PingServer()
+	{
+		message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ServerPing;
+
+		// Caution with this...
+		std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+
+		msg << timeNow;
+		Send(msg);
+	}
+
+	void MessageAll()
+	{
+		message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::MessageAll;
+		Send(msg);
+	}
 };
 
-void InputHandler(bool& bQuit, CustomClient& c) {
-    while (!bQuit) {
-        std::string inputLine;
-        std::getline(std::cin, inputLine);
-        for (char ch : inputLine) {
-            if (ch == '1') {
-                c.PingServer();
-            }
-            else if (ch == '2') {
-                c.MessageAll();
-            }
-            else if (ch == '3') {
-                bQuit = true;
-                break;
-            }
-        }
-    }
-}
+int main()
+{
+	CustomClient c;
+	c.Connect("127.0.0.1", 60000);
 
-int main() {
-    CustomClient c;
-    if (c.Connect("127.0.0.1", 60000)) {
-        std::cout << "Connected to server.\n";
-    }
-    else {
-        std::cout << "Failed to connect to server.\n";
-        return 1;
-    }
+	bool key[3] = { false, false, false };
+	bool old_key[3] = { false, false, false };
 
-    bool bQuit = false;
-    std::thread inputThread(InputHandler, std::ref(bQuit), std::ref(c));
+	bool bQuit = false;
+	while (!bQuit)
+	{
+		std::cout << "Enter command (1: Ping Server, 2: Message All, 3: Quit): ";
+		std::string inputLine;
+		std::getline(std::cin, inputLine);
 
-    while (!bQuit) {
-        if (c.IsConnected()) {
-            if (!c.Incoming().empty()) {
-                std::cout << "Message received in client.\n"; // Debug message
-                auto msg = c.Incoming().pop_front().msg;
-                switch (msg.header.id) {
-                case CustomMsgTypes::ServerPing:
-                {
-                    std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
-                    std::chrono::system_clock::time_point timeThen;
-                    msg >> timeThen;
-                    std::cout << "RTT: " << std::chrono::duration<double>(timeNow - timeThen).count() << " seconds\n";
-                }
-                break;
-                case CustomMsgTypes::ServerAccept: {
-                    std::cout << "Server has accepted the id\n";
-                }
-                break;
-                case CustomMsgTypes::ServerMessage: {
-                    uint32_t clientId;
-                    msg >> clientId;
-                    std::cout << "Hello From [" << clientId << "]\n";
-                }
-                break;
-                default:
-                    std::cout << "Received unknown message ID: " << static_cast<uint32_t>(msg.header.id) << "\n";
-                    break;
-                }
-            }
-            else {
-                // Optional sleep to prevent tight looping
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            }
-        }
-        else {
-            std::cout << "Server Down\n";
-            bQuit = true;
-        }
-    }
+		for (char ch : inputLine)
+		{
+			if (ch == '1')
+			{
+				c.PingServer();
+			}
+			else if (ch == '2')
+			{
+				c.MessageAll();
+			}
+			else if (ch == '3')
+			{
+				bQuit = true;
+				break;
+			}
+			else
+			{
+				std::cout << "Unknown command: " << ch << "\n";
+			}
+		}
+		if (c.IsConnected())
+		{
+			if (!c.Incoming().empty())
+			{
 
-    inputThread.join();
-    return 0;
+
+				auto msg = c.Incoming().pop_front().msg;
+
+				switch (msg.header.id)
+				{
+				case CustomMsgTypes::ServerAccept:
+				{
+					// Server has responded to a ping request				
+					std::cout << "Server Accepted Connection\n";
+				}
+				break;
+
+
+				case CustomMsgTypes::ServerPing:
+				{
+					// Server has responded to a ping request
+					std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+					std::chrono::system_clock::time_point timeThen;
+					msg >> timeThen;
+					std::cout << "Ping: " << std::chrono::duration<double>(timeNow - timeThen).count() << "\n";
+				}
+				break;
+
+				case CustomMsgTypes::ServerMessage:
+				{
+					// Server has responded to a ping request	
+					uint32_t clientID;
+					msg >> clientID;
+					std::cout << "Hello from [" << clientID << "]\n";
+				}
+				break;
+				}
+			}
+		}
+		else
+		{
+			std::cout << "Server Down\n";
+			bQuit = true;
+		}
+
+	}
+
+	return 0;
 }
