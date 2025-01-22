5ac4cc56cd78976de9c5067488c1968cc1dadd8c
Dhruva Sharma <dhruva1098@gmail.com>
2025-01-22 12:40:40 +0530

    basic files

--- Networking/GameClient/GameClient.cpp
+++ Networking/GameClient/GameClient.cpp
@@ -1,4 +1,24 @@
 #include <network_system.h>
+
+#include "../GameServer/game_common.h"
 #include "olcPixelGameEngine.h"
 #include "extensions/olcPGEX_TransformedView.h"
 
+class Game : public olc::PixelGameEngine, client_interface<GameMsg> {
+public:
+	Game() {
+		sAppName = "GameClient";
+	}
+
+public:
+	bool OnUserCreate() override {
+		return true;
+	}
+	bool OnUserUpdate(float fElapsedTime) override {
+		return true;
+	}
+};
+
+int main() {
+	return 0;	
+}
---
+++ Networking/GameServer/game_common.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdint>
+
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+#define NETWORKIN_SYSTEM
+#include <network_system.h>
