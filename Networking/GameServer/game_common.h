8addf92e940cc8224e8b2d71ee480209275e14b1
Dhruva Sharma <dhruva1098@gmail.com>
2025-01-22 13:19:58 +0530

    packetStructure

--- Networking/GameServer/game_common.h
+++ Networking/GameServer/game_common.h
@@ -6,3 +6,17 @@
 
 #define NETWORKIN_SYSTEM
 #include <network_system.h>
+
+enum class GameMsg :uint32_t {
+	Server_GetStatus,
+	Server_GetPing,
+
+	Client_Accepted,
+	Client_AssignID,
+	Client_RegisterWithServer,
+	Client_UnregisterWithServer,
+
+	Game_AddPlayer,
+	Game_RemovePlayer,
+	Game_UpdatePlayer,
+};
